c2e244689c21e95e760b0f1b4825d88867d6e12e
KenUTS <111736067+KenUTS@users.noreply.github.com>
2023-10-10 18:14:34 +1100

    heroku

--- app/main.py
+++ app/main.py
@@ -7,15 +7,15 @@ import pandas as pd
 
 app = FastAPI()
 
-SARI_model = load('../models/SARIMAX.joblib')
+# SARI_model = load('../models/SARIMAX.joblib')
 
-# FOODS_1_df_dt_pipeline = load('../models/FOODS_1_df_dt_pipeline.joblib')
-# FOODS_2_df_dt_pipeline = load('../models/FOODS_2_df_dt_pipeline.joblib')
-# FOODS_3_df_dt_pipeline = load('../models/FOODS_3_df_dt_pipeline.joblib')
-# HOBBIES_1_df_dt_pipeline = load('../models/HOBBIES_1_df_dt_pipeline.joblib')
-# HOBBIES_2_df_dt_pipeline = load('../models/HOBBIES_2_df_dt_pipeline.joblib')
-# HOUSEHOLD_1_df_dt_pipeline = load('../models/HOUSEHOLD_1_df_dt_pipeline.joblib')
-# HOUSEHOLD_2_df_dt_pipeline = load('../models/HOUSEHOLD_2_df_dt_pipeline.joblib')
+FOODS_1_df_dt_pipeline = load('../models/FOODS_1_df_dt_pipeline.joblib')
+FOODS_2_df_dt_pipeline = load('../models/FOODS_2_df_dt_pipeline.joblib')
+FOODS_3_df_dt_pipeline = load('../models/FOODS_3_df_dt_pipeline.joblib')
+HOBBIES_1_df_dt_pipeline = load('../models/HOBBIES_1_df_dt_pipeline.joblib')
+HOBBIES_2_df_dt_pipeline = load('../models/HOBBIES_2_df_dt_pipeline.joblib')
+HOUSEHOLD_1_df_dt_pipeline = load('../models/HOUSEHOLD_1_df_dt_pipeline.joblib')
+HOUSEHOLD_2_df_dt_pipeline = load('../models/HOUSEHOLD_2_df_dt_pipeline.joblib')
 
 project_obs= "The assigned objective is to develop two distinct models that will be implemented as APIs in a production environment. This study proposes the development of a prediction model utilising a Machine Learning algorithm to effectively forecast the sales income of a particular item within a designated retailer on a specific day. This study proposes the use of a time-series analysis method to develop a forecasting model for predicting the aggregate sales income of all stores and items throughout the forthcoming seven-day period."
 
@@ -35,65 +35,65 @@ def read_root():
 def healthcheck():
     return 'Welcome!'
 
-@app.get("/sales/national/")
-def forecasting(
-    date: str
+# @app.get("/sales/national/")
+# def forecasting(
+#     date: str
+#     ):
+#     date_input = datetime.strptime(date, "%Y-%m-%d")
+#     date_seven=(date_input + timedelta(days=7)).strftime('%Y-%m-%d')
+#     date_one=(date_input + timedelta(days=1)).strftime('%Y-%m-%d')
+#     sobs = SARI_model.get_prediction(start=date_one, end=date_seven).predicted_mean
+#     range = pd.date_range(start=date_one, end=date_seven).to_list()
+#     date_ranges = [str(d.strftime('%Y-%m-%d')) for d in range]
+#     forecast_list = {date: value for date, value in zip(date_ranges, sobs)}
+#     return JSONResponse(forecast_list)
+
+def format_features_predictive(
+    date: str,
+    item_id: str,
+    store_id: str,
+    is_event: int
     ):
-    date_input = datetime.strptime(date, "%Y-%m-%d")
-    date_seven=(date_input + timedelta(days=7)).strftime('%Y-%m-%d')
-    date_one=(date_input + timedelta(days=1)).strftime('%Y-%m-%d')
-    sobs = SARI_model.get_prediction(start=date_one, end=date_seven).predicted_mean
-    range = pd.date_range(start=date_one, end=date_seven).to_list()
-    date_ranges = [str(d.strftime('%Y-%m-%d')) for d in range]
-    forecast_list = {date: value for date, value in zip(date_ranges, sobs)}
-    return JSONResponse(forecast_list)
+    return {
+        'date': [date],
+        'item_id': [item_id],
+        'store_id': [store_id],
+        'is_event': [is_event]
+    }
 
-# def format_features_predictive(
-#     date: str,
-#     item_id: str,
-#     store_id: str,
-#     is_event: int
-#     ):
-#     return {
-#         'date': [date],
-#         'item_id': [item_id],
-#         'store_id': [store_id],
-#         'is_event': [is_event]
-#     }
+def predictive_model(item_id):
+    if 'FOODS_1' in item_id:
+        return FOODS_1_df_dt_pipeline
+    elif 'FOODS_2' in item_id:
+        return FOODS_2_df_dt_pipeline
+    elif 'FOODS_3' in item_id:
+        return FOODS_3_df_dt_pipeline
+    elif 'HOBBIES_1' in item_id:
+        return HOBBIES_1_df_dt_pipeline
+    elif 'HOBBIES_2' in item_id:
+        return HOBBIES_2_df_dt_pipeline
+    elif 'HOUSEHOLD_1' in item_id:
+        return HOUSEHOLD_1_df_dt_pipeline
+    elif 'HOUSEHOLD_2' in item_id:
+        return HOUSEHOLD_2_df_dt_pipeline
 
-# def predictive_model(item_id):
-#     if 'FOODS_1' in item_id:
-#         return FOODS_1_df_dt_pipeline
-#     elif 'FOODS_2' in item_id:
-#         return FOODS_2_df_dt_pipeline
-#     elif 'FOODS_3' in item_id:
-#         return FOODS_3_df_dt_pipeline
-#     elif 'HOBBIES_1' in item_id:
-#         return HOBBIES_1_df_dt_pipeline
-#     elif 'HOBBIES_2' in item_id:
-#         return HOBBIES_2_df_dt_pipeline
-#     elif 'HOUSEHOLD_1' in item_id:
-#         return HOUSEHOLD_1_df_dt_pipeline
-#     elif 'HOUSEHOLD_2' in item_id:
-#         return HOUSEHOLD_2_df_dt_pipeline
-
-# @app.get("/sales/stores/items/")
-# def predict(
-#     date: str,
-#     item_id: str,
-#     store_id: str,
-#     is_event: int =0
-#     ):
-#     features = format_features_predictive(
-#         date,
-#         item_id,
-#         store_id,
-#         is_event
-#         )
-#     obs = pd.DataFrame(features)
-#     obs['date'] = pd.to_datetime(obs['date'])
-#     obs['day_of_month'] = obs['date'].dt.day
-#     obs['month_of_year'] = obs['date'].dt.month
-#     obs['day_of_week'] = obs['date'].dt.dayofweek
-#     pred = predictive_model(item_id).predict(obs.drop(columns=['date']))
-#     return JSONResponse(pred.tolist())
+@app.get("/sales/stores/items/")
+def predict(
+    date: str,
+    item_id: str,
+    store_id: str,
+    is_event: int =0
+    ):
+    features = format_features_predictive(
+        date,
+        item_id,
+        store_id,
+        is_event
+        )
+    obs = pd.DataFrame(features)
+    obs['date'] = pd.to_datetime(obs['date'])
+    obs['day_of_month'] = obs['date'].dt.day
+    obs['month_of_year'] = obs['date'].dt.month
+    obs['day_of_week'] = obs['date'].dt.dayofweek
+    pred = predictive_model(item_id).predict(obs.drop(columns=['date']))
+    return JSONResponse(pred.tolist())
